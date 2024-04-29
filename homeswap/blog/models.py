113230ed59586f5ecb6bd8e0ebd0bf66d3ae8765
RobertoGM5 <robertogmvfx@gmail.com>
2024-04-29 13:50:03 +0200

    first version blog app

--- homeswap/blog/models.py
+++ homeswap/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
 
-# Create your models here.
+class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    to_city = models.CharField(max_length=100)
+    date_period = models.DateRangeField()
+    num_travelers = models.PositiveIntegerField()
+    description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.from_city:
+            self.from_city = self.user.userprofile.city
+        super().save(*args, **kwargs)
--- homeswap/blog/views.py
+++ homeswap/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import BlogPostForm
 
-# Create your views here.
+@login_required
+def create_blog_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            blog_post = form.save(commit=False)
+            blog_post.user = request.user
+            blog_post.save()
+            return redirect('blog_home')  
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/create_blog_post.html', {'form': form})
