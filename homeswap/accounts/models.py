75400c8e5a579dadf328e850228bae607920d801
SangDoo Nam <kexe009@hotmail.com>
2024-05-22 01:18:49 +0200

    test accounts models

--- homeswap/accounts/models.py
+++ homeswap/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -23,15 +24,34 @@ class AppUser(AbstractUser):
     profile_photo = models.ImageField(upload_to='profile_photo', default='profile_photo/default.jpg')
     phone_number = PhoneNumberField(null=True, blank=True, region='DE')
     biography = models.TextField(null=True, blank=True)
-    street = models.CharField(max_length=100, null=True, blank=True)
-    location = models.CharField(max_length=50, null=True, blank=True)
+    street = models.CharField(max_length=40, null=True, blank=True)
+    location = models.CharField(max_length=40, null=True, blank=True)
     max_capacity = models.SmallIntegerField(null=True, blank=True)
-    postal_code = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(999999)], null=True, blank=True)
+    postal_code = models.CharField(
+        max_length=5, 
+        null=True, 
+        blank=True, 
+        validators=[RegexValidator(r'^\d{5}$', 'Postal code should contain exactly 5 digits')]
+    )
     birthday_date = models.DateField(null=True, blank=True)
     
     def __str__(self) -> str:
         return self.username
 
+    def clean(self):
+        if len(self.street) > 40:
+            raise ValidationError({'street': 'street can contain not more 40 letters.'})
+        
+        if len(self.location) > 40:
+            raise ValidationError({'location': 'location can contain not more 40 letters.'})
+        
+        if self.postal_code and not self.postal_code.isdigit():
+            raise ValidationError({'postal_code': 'Postal code should contain exactly 5 digits'})
+        
+        if len(self.postal_code) > 5:
+            raise ValidationError({'postal_code': 'Postal code should contain exactly 5 digits'})
+        
+        super().clean()
 
 class HomePhoto(ValidateModel):
     
---
+++ homeswap/accounts/tests/model_test.py
@@ -0,0 +1,106 @@
+from django.test import TestCase
+from django.core.files import File
+from django.core.exceptions import ValidationError
+from django.apps import apps
+from django.db import models as db
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+class Test(TestCase):
+
+    def setUp(self) -> None:
+        """Set up."""
+        super().setUp()
+
+        self.appuser = self.get_model('AppUser')
+        self.homephoto = self.get_model('HomePhoto')
+
+        profile_photo = open(BASE_DIR / "default.jpg", 'rb')
+        home_photo = open(BASE_DIR / 'test_home_photo_1.jpg', 'rb')
+
+        self.appuser_params = {
+            'username': 'unique_user',  # Ensure the username is unique
+            'email': 'user@email.com',
+            'profile_photo': File(profile_photo, name='default.jpg'),
+            'phone_number': '+4917633344234',
+            'street': 'Beispielstraße',
+            'location': 'Berlin',
+            'max_capacity': 2,
+            'postal_code': 12345,
+        }
+
+        self.homephoto_params = {
+            'user': self.appuser.objects.create(**self.appuser_params),
+            'image': File(home_photo, name='test_home_photo_1.jpg'),
+            'photo_type': 'bed room',
+        }
+
+    def get_model(self, model_name=None):
+        if not model_name:
+            model_name = self.model_name
+        try:
+            model = apps.get_model('accounts', model_name)
+        except LookupError:
+            model = None
+        return model
+
+    def test_model_names(self):
+        self.assertIsNotNone(self.appuser)
+        self.assertIsNotNone(self.homephoto)
+
+    def test_required_fields(self):
+
+        with self.assertRaises(ValidationError) as cm:
+            self.homephoto.objects.create()
+        e = cm.exception
+        fields = [error[0] for error in e]
+        self.assertEqual(len(fields), 2)
+        self.assertIn('user', fields)
+        self.assertIn('photo_type', fields)
+
+    def test_postal_code(self):
+        params = self.appuser_params.copy()
+        params['postal_code'] = 'ABCDE'
+        params['username'] = 'unique_user_postal_code'  
+        with self.assertRaises(ValidationError) as cm:
+            user = self.appuser(**params)
+            user.clean()
+        e = cm.exception
+        fields = [error[0] for error in e]
+        self.assertIn('postal_code', fields)
+        
+        params['postal_code'] = '123456'  
+        with self.assertRaises(ValidationError) as cm:
+            user = self.appuser(**params)
+            user.clean()
+        e = cm.exception
+        fields = e.error_dict.keys()
+        self.assertIn('postal_code', fields)
+
+    def test_string_lengths(self):
+        params = self.appuser_params.copy()
+        params['street'] = 'a' * 41  
+        params['username'] = 'unique_user_string_lengths' 
+
+        with self.assertRaises(ValidationError) as cm:
+            user = self.appuser(**params)
+            user.clean()
+        e = cm.exception
+        fields = e.error_dict.keys()
+        self.assertIn('street', fields)
+        
+        params = self.appuser_params.copy()
+        params['location'] = 'a' * 41 
+        params['username'] = 'nice_user_name'
+        
+        with self.assertRaises(ValidationError) as cm:
+            user = self.appuser(**params)
+            user.clean()
+        e = cm.exception
+        fields = e.error_dict.keys()
+        self.assertIn('location', fields)
+
+    def test_foreign_keys(self):
+        field = self.homephoto._meta.get_field("user")
+        self.assertTrue(isinstance(field, db.ForeignKey))
