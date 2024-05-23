7e8bc1e1e6a4698f9a9257f2c7879b4641a798db
SangDoo Nam <kexe009@hotmail.com>
2024-05-23 15:14:06 +0200

    test blog application

--- homeswap/blog/models.py
+++ homeswap/blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
-from accounts.models import AppUser
+from django.core.exceptions import ValidationError
 
+from accounts.models import AppUser
+import datetime as dt
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=100, default='Untitled')
@@ -15,3 +17,16 @@ class BlogPost(models.Model):
     description = models.TextField()
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
+
+    def clean(self) -> None:
+        
+        if self.start_date < dt.date.today():
+            raise ValidationError({'start_date': "Start date can't be older then today"})
+        
+        if self.end_date and self.end_date < dt.date.today():
+            raise ValidationError({'end_date': "End date can't be older then today"})
+        
+        if self.num_travelers < 1:
+            raise ValidationError({'num_travelers': "Number of travelers needs to be at least 1."})
+        
+        super().clean()
--- homeswap/blog/tests/model_test.py
+++ homeswap/blog/tests/model_test.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from django.core.files import File
 from django.core.exceptions import ValidationError
 from django.apps import apps
-from django.db import models as db
 from pathlib import Path
-from django.utils import timezone
+
 from datetime import datetime, timedelta
+import datetime as dt
 # Create your tests here.
 
 
@@ -43,9 +43,6 @@ class Test(TestCase):
             'num_travelers' : 4,
             'description': 'test description',
         }
-        
-    
-    
     
     def get_model(self, model_name=None):
         if not model_name:
@@ -61,4 +58,115 @@ class Test(TestCase):
     
     
     def test_required_fields(self):
-        pass
+        params = self.appuser_params.copy()
+        params['username'] = 'unique_user_required_fields'
+        params['email'] = 'unique@email.com'
+        params['phone_number'] = '+4917623473834'
+        
+        user = self.appuser.objects.create(**params)
+        blogpost_params = self.blogpost_params.copy()
+        blogpost_params['user'] = user
+        blogpost_params['title'] = None
+        blogpost_params['location'] = None
+        blogpost_params['max_capacity'] = None
+        blogpost_params['to_city'] = None
+        blogpost_params['end_date'] = None
+        blogpost_params['description'] = None
+
+        blogpost = self.blogpost(**blogpost_params)
+        with self.assertRaises(ValidationError) as cm:
+            blogpost.full_clean()
+
+        e = cm.exception
+        fields = e.message_dict.keys()
+        self.assertIn('title', fields)
+        self.assertIn('location', fields)
+        self.assertIn('max_capacity', fields)
+        self.assertIn('to_city', fields)
+        self.assertIn('end_date', fields)
+        self.assertIn('description', fields)
+    
+    def test_max_length_validation(self):
+        
+        params = self.appuser_params.copy()
+        params['username'] = 'unique_user_length_validation'
+        params['email'] = 'length@email.com'
+        params['phone_number'] = '+4917623343834'
+        
+        user = self.appuser.objects.create(**params)
+        blogpost_params = self.blogpost_params.copy()
+        blogpost_params['user'] = user
+        blogpost_params['location'] = 'a' * 101
+        blogpost_params['to_city'] = 'b' * 101
+        blogpost = self.blogpost(**blogpost_params)
+        with self.assertRaises(ValidationError) as cm:
+            blogpost.full_clean()
+        
+        e = cm.exception
+        fields = e.message_dict.keys()
+        self.assertIn('location', fields)
+        self.assertIn('to_city', fields)
+    
+    def test_start_date(self):
+        
+        params = self.appuser_params.copy()
+        params['username'] = 'unique_user_start_date'
+        params['email'] = 'start@email.com'
+        params['phone_number'] = '+4917623773834'
+        
+        today = dt.date.today()
+        older_then_today = today - timedelta(days=7)
+        
+        user = self.appuser.objects.create(**params)
+        blogpost_params = self.blogpost_params.copy()
+        blogpost_params['user'] = user
+        blogpost_params['start_date'] = older_then_today
+        
+        blogpost = self.blogpost(**blogpost_params)
+        with self.assertRaises(ValidationError) as cm:
+            blogpost.full_clean()
+        
+        e = cm.exception
+        fields = e.message_dict.keys()
+        self.assertIn('start_date', fields)
+        
+    def test_end_date(self):
+        
+        params = self.appuser_params.copy()
+        params['username'] = 'unique_user_end_date'
+        params['email'] = 'end@email.com'
+        params['phone_number'] = '+4912323773834'
+        
+        today = dt.date.today()
+        older_then_today = today - timedelta(days=7)
+        
+        user = self.appuser.objects.create(**params)
+        blogpost_params = self.blogpost_params.copy()
+        blogpost_params['user'] = user
+        blogpost_params['end_date'] = older_then_today
+        
+        blogpost = self.blogpost(**blogpost_params)
+        with self.assertRaises(ValidationError) as cm:
+            blogpost.full_clean()
+        
+        e = cm.exception
+        fields = e.message_dict.keys()
+        self.assertIn('end_date', fields)
+        
+    def test_num_travelers(self):
+        params = self.appuser_params.copy()
+        params['username'] = 'unique_user_num_travelers'
+        params['email'] = 'travelers@email.com'
+        params['phone_number'] = '+4917623712834'
+        user = self.appuser.objects.create(**params)
+        blogpost_params = self.blogpost_params.copy()
+        blogpost_params['user'] = user
+        blogpost_params['num_travelers'] = 0
+        
+        blogpost = self.blogpost(**blogpost_params)
+        with self.assertRaises(ValidationError) as cm:
+            blogpost.full_clean()
+        
+        e = cm.exception
+        fields = e.message_dict.keys()
+        self.assertIn('num_travelers', fields)
