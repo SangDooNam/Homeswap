8f66a36dfd4bc446e4b6530a42dbd8d2d0448e05
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2024-05-28 10:26:29 +0200

    added path for new api view

--- homeswap/search/urls.py
+++ homeswap/search/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/search/', search_view, name='search'),
     path('form/', search_form_view, name='search_form'),
     path('api/blog/<int:post_id>/', blog_post_details_view, name='blog_post_details'),
+    path('api/search/blog-posts/', api_search_blog_posts_view, name='api_search_blog_posts'),
 ]
